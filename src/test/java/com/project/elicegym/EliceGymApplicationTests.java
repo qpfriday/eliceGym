8024f0b0b985b0bbb0ae25ef29f0372553b66626
chan <qpfriday@gmail.>
2024-03-26 11:13:41 +0900

    first commit

---
+++ src/test/java/com/project/elicegym/EliceGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.elicegym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliceGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
