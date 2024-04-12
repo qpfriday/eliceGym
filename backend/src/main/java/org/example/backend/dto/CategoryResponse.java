65dcef372c0b5ea06e1e7ff41656b6fc02d328f2
tunacan0329 <tunacan0329@gmail.com>
2024-04-13 04:20:31 +0900

    Fix: 카테고리 조회 dto 수정

--- backend/src/main/java/org/example/backend/dto/CategoryResponse.java
+++ backend/src/main/java/org/example/backend/dto/CategoryResponse.java
@@ -5,12 +5,14 @@ import org.example.backend.entity.Category;
 @Getter
 public class CategoryResponse {
 
+    private final int id;
     private final String name;
     private final String description;
     private final String createdAt;
     private final String updatedAt;
 
     public CategoryResponse(Category category){
+        this.id = category.getId();
         this.name = category.getName();
         this.description = category.getDescription();
         this.createdAt = category.getCreatedAt();
