1ab26e1429e2a6559123c088397cec95cf062df5
tunacan0329 <tunacan0329@gmail.com>
2024-04-15 21:43:36 +0900

    Add: 카테고리 수정 후 목록으로 돌아가기

--- frontend/src/pages/ModifyCategory.vue
+++ frontend/src/pages/ModifyCategory.vue
@@ -48,7 +48,7 @@ export default {
       axios.put(`/api/categories/${categoryId.value}`, state.value)
           .then(() => {
             console.log("Category updated successfully");
-            router.push('/');  // 수정 후 목록 페이지로 이동
+            router.push('/categoryManagement');  // 수정 후 목록 페이지로 이동
           })
           .catch(error => {
             console.error("Failed to update category:", error);
