2a281849c8e65556eb4603a978f7d97baecf98f6
tunacan0329 <tunacan0329@gmail.com>
2024-04-19 04:41:38 +0900

    Fix:상품 삭제 안되는 문제 해결

--- frontend/src/pages/ProductDetail.vue
+++ frontend/src/pages/ProductDetail.vue
@@ -93,7 +93,7 @@ export default {
 
     const deleteItem = () => {
       axios
-        .delete(`/api/item/${itemId}/delete`)
+        .delete(`/api/item/${itemId}`)
         .then(() => {
           alert("상품이 삭제되었습니다.");
           router.push(ROUTER_LINKS.HOME.path); // 삭제 후 홈 페이지로 이동
