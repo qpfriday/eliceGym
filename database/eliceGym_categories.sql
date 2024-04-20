e8da2c152d2470cfa054f8341f5da75aefa39be6
chan <qpfriday@gmail.>
2024-04-20 10:53:03 +0900

    Fix: categori db 쿼리 수정

--- database/eliceGym_categories.sql
+++ database/eliceGym_categories.sql
@@ -1,11 +1,11 @@
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (1, '티셔츠', '옷입자', null, null);
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (2, '단백질', '약먹자', null, null);
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (3, '스트랩', '장비빨', null, null);
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (4, '장갑', '장갑', '', null);
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (5, '신발', '신발', '', null);
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (6, '양말', '양말', '', null);
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (7, '모자', '모자', null, null);
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (8, '닭가슴살', '닭가슴살', null, null);
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (9, '바지', '바지', null, null);
-INSERT INTO elice_db.category (id, name, description, created_at, updated_at) VALUES (10, '크레아틴', '크레아틴', null, null);
-INSERT INTO eliceGym.category (id, name, description, created_at, updated_at) VALUES (11, '벨트', '벨트', null, null);
+INSERT INTO elice_db.category (id, name, description, created_at, updated_at)
+VALUES (1, '티셔츠', '옷입자', null, null),
+       (2, '단백질', '약먹자', null, null),
+       (3, '스트랩', '장비빨', null, null),
+       (4, '장갑', '장갑', null, null),
+       (5, '신발', '신발', null, null),
+       (6, '양말', '양말', null, null),
+       (7, '모자', '모자', null, null),
+       (9, '바지', '바지', null, null),
+       (10, '크레아틴', '크레아틴', null, null),
+       (11, '벨트', '벨트', null, null);
